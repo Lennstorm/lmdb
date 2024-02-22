0a1a03445dcc5316d04cd29ac11e9033935d4e6f
Lennstorm <4824136@gmail.com>
2024-02-22 13:04:22 +0100

    random filmer funkar

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 'use strict';
 
 window.addEventListener('load', () => {
-    console.log('load');
+    console.log('c.log load');
     //Förslagsvis anropar ni era funktioner som skall sätta lyssnare, rendera objekt osv. härifrån
+    processMovies(); 
+    fetchDataAndStore();
     setupCarousel();
+    upDateTrailers();
 });
 
 //Denna funktion skapar funktionalitet för karusellen
 function setupCarousel() {
-    console.log('carousel');
+    console.log('c.log carousel');
     const buttons = document.querySelectorAll('[data-carousel-btn]');
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -27,4 +30,73 @@ function setupCarousel() {
             delete activeSlide.dataset.active;
         });
     });
+}
+
+// Function för att hämta data från Jespers API
+
+async function fetchMovies() {
+ try {
+    const response = await fetch('https://santosnr6.github.io/Data/movies.json');
+    const movies = await response.json();
+    console.log('c.log success fetching movies!');
+    return movies;
+ } catch(error) {
+     console.log('c.log  error fetching data', error);
+     return [];
+    }
+}
+
+async function processMovies() {
+    try {
+        const movieData = await fetchMovies();
+        console.log('c.log Filmdata', movieData)
+
+    } catch (error) {
+        console.log('c.log Error processing data', error);
+    }
+}
+
+
+// Global variabel som lagrar data
+let movieData;
+
+async function fetchDataAndStore() {
+    try {
+        movieData = await fetchMovies();
+        console.log('c.log Fetched movie däta', movieData);        
+    } catch (error){
+        console.error('ärror fetching däta', error)
+    }
+}
+
+// Slumpgenerator som tar fram fem random filmer och lägger dem i karusellen
+
+async function upDateTrailers() {
+    try {
+        const movies = await fetchMovies();
+        const randomMovies = [];
+        const usedIndexes = new Set();
+        
+        while (randomMovies.length < 5) {
+            const randomIndex = Math.floor(Math.random() * movies.length);
+            if (!usedIndexes.has(randomIndex)) {
+                randomMovies.push(movies[randomIndex]);
+                usedIndexes.add(randomIndex);
+            }
+        }        
+        
+        console.log('* * * * * Slumpade filmer:');
+        randomMovies.forEach(movie => {
+            console.log('* *', movie.title);
+        });
+
+        const iframes = document.querySelectorAll('.carousel__slide iframe');
+        iframes.forEach((iframe, index) => {
+            iframe.src = randomMovies[index].trailer_link;
+        })
+
+        console.log('c.log random filmer har lagts till i DOM');
+    }   catch (error) {
+        console.log('c.log din jävla klant du har kodat fel!', error);
+    }
 }
